25ae26fed305162b3c4f7d8516ba10ffff7798f6
Steph Peets <stephanie.peets@gmail.com>
2021-10-07 23:07:39 -0400

    submit class 5 studio

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,35 +1,55 @@
 package main;
 
-
 public class BalancedBrackets {
     /**
-     * The function BalancedBrackets should return true if and only if
-     * the input string has a set of "balanced" brackets.
+     * The function BalancedBrackets should return true if and only if the input
+     * string has a set of "balanced" brackets.
      *
-     * That is, whether it consists entirely of pairs of opening/closing
-     * brackets (in that order), none of which mis-nest. We consider a bracket
-     * to be square-brackets: [ or ].
+     * That is, whether it consists entirely of pairs of opening/closing brackets
+     * (in that order), none of which mis-nest. We consider a bracket to be
+     * square-brackets: [ or ].
      *
      * The string may contain non-bracket characters as well.
      *
-     * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     * These strings have balanced brackets: "[LaunchCode]", "Launch[Code]",
+     * "[]LaunchCode", "", "[]"
      *
-     * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     * While these do not: "[LaunchCode", "Launch]Code[", "[", "]["
      *
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
-        for (char ch : str.toCharArray()) {
-            if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
-            }
+
+        int firstOpenBracket = str.indexOf('[');
+        int firstCloseBracket = str.indexOf("]");
+
+        int lastOpenBracket = str.lastIndexOf('[');
+        int lastCloseBracket = str.lastIndexOf("]");
+
+        boolean noClosingBracket = firstCloseBracket == -1;
+        boolean noOpeningBracket = firstOpenBracket == -1;
+
+        boolean evenOpenOneCloseBrackets = (
+            firstOpenBracket != lastOpenBracket && 
+            firstCloseBracket == lastCloseBracket
+        );
+
+        boolean evenCloseOneOpenBrackets = (
+            firstCloseBracket != lastCloseBracket && 
+            firstOpenBracket == lastOpenBracket
+        );
+
+        boolean answer;
+        if (noClosingBracket || noOpeningBracket) {
+            answer = false;
+        } else if (evenOpenOneCloseBrackets || evenCloseOneOpenBrackets) {
+            answer = false;
+        } else {
+            answer = lastOpenBracket < lastCloseBracket;
         }
-        return brackets == 0;
+
+        return answer;
     }
+
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -2,15 +2,112 @@ package test;
 
 import org.junit.Test;
 
+import main.BalancedBrackets;
+
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
 
+    @Test
+    public void balancedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void wrongOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void twoOpenReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void twoClosedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
+    }
+
+    @Test
+    public void emptyStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void stringEnclosedInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[inBrackets]"));
+    }
+
+    @Test
+    public void singleOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void singleCloseBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
+    }
+
+    @Test
+    public void evenOpenOddCloseReturnsFalse() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("[brac[kets]"));
+    }
+
+    @Test
+    public void evenCloseOddOpenReturnsFalse() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("[onetwothr]eefour]"));
+    }
+
+
+    @Test
+    public void twoBalancedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[one]two[threefour]"));
+    }
+
+    @Test
+    public void twoUnbalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]one]two[threefour["));
+    }
+
+    @Test
+    public void threeBalancedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[one]two[three][four]"));
+    }
+
+    @Test
+    public void threeUnbalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]one]two][[threefour["));
+    }
+
+
+    @Test
+    public void oddNumOfMostlyUnbalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]o]e]tw][[o][[[[[[[t]]]hr]]]eef][[[our["));
+    }
+
+    @Test
+    public void evenNumOfMostlyUnbalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]o]e]tw][[o][[[[[[[t]]]hr]]]eef][[[our[["));
+    }                                            
+
+    @Test
+    public void manyBalancedBracketsSet() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[o]e]tw][[o]][[[[[[t]]]hr]]]eef[][[our]"));
+    }   
+    
+    @Test
+    public void largeNumBracketsAllUnbalancedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]]]]]]]]]]]]]][[[[[[[[[[[[[[[["));
+    } 
+
+    public void largeNumBracketsAllBalancedReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]"));
+    } 
+    
+     
 }
